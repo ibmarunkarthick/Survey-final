c6fd5b4ae1d85b95a72cd49dad3b8c0c3564d9c4
ibmarunkarthick <ibmarun94@gmail.com>
2021-07-21 16:15:01 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,14 @@ import { CreateSurveyComponent } from './create-survey/create-survey.component';
 import { MaterialModule } from './material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule,BrowserAnimationsModule, FormsModule,MaterialModule,ReactiveFormsModule ],
+  imports:      [ BrowserModule,BrowserAnimationsModule, FormsModule,MaterialModule,ReactiveFormsModule,RouterModule.forRoot([
+    { path: 'create', component:CreateSurveyComponent },
+    {path:'home',component:HomeComponent},
+     { path: '', redirectTo: '/home', pathMatch: 'full' }
+  ], { useHash: true }), ],
   declarations: [ AppComponent, HelloComponent,CreateSurveyComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
